51b22855f4557c3921e1a31a4aae326009a4660a
Juno9170 <ryanzhuturtle@gmail.com>
2024-03-30 13:50:43 -0400

    Initial Commit, installing dependencies

--- proj/App.js
+++ proj/App.js
@@ -1,14 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useState} from 'react';
+import Map from './Map';
 
-export default function App() {
+
+export default App = () => {
+  const [test,setTest] = useState("hello");
+  
+  const change = () =>{
+    setTest("Hello World!")
+  }
+  
   return (
     <View style={styles.container}>
+      <Map/>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>is this working ajsdaksdklj</Text>
       <Text>klxedj</Text>
-      <Text>asdasda</Text>
+      <Text>a</Text>
+      <Text>{test}</Text>
+      <Button title="Change Text" onPress={change} />
+      <Map/>
       <StatusBar style="auto" />
+      
     </View>
   );
 }
---
+++ proj/Map.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+import MapboxGL from "@rnmapbox/maps";
+
+MapboxGL.setAccessToken("<YOUR_ACCESSTOKEN>");
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  container: {
+    height: 300,
+    width: 300,
+    backgroundColor: "tomato"
+  },
+  map: {
+    flex: 1
+  }
+});
+
+export default class Map extends Component {
+  componentDidMount() {
+    MapboxGL.setTelemetryEnabled(false);
+  }
+
+  render() {
+    return (
+      <View style={styles.page}>
+        <View style={styles.container}>
+          <MapboxGL.MapView style={styles.map} />
+        </View>
+      </View>
+    );
+  }
+}
