7c1abfdc27966493a8967651bdb17cba1f9f89d8
Juno9170 <ryanzhuturtle@gmail.com>
2024-03-30 22:47:37 -0400

    History Graphics done

--- proj/assets/Pages/History.js
+++ proj/assets/Pages/History.js
@@ -1,33 +1,106 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text ,ScrollView } from 'react-native';
 import Calendar from './Calendar';
 export default function History() {
   return (
-    <View style={styles.container}>
+    <ScrollView 
+      contentContainerStyle={styles.container}
+      showsVerticalScrollIndicator={false} // Optionally hide the vertical scroll indicator
+    >
+      <View style = {styles.calendarContainer}>
+        <Calendar
+          year= {2024}
+          month='January'
+        />
+      </View>
       <View style = {styles.calendarContainer}>
         <Calendar
           year= {2024}
           month='February'
         />
       </View>
-    </View>
+
+      <View style = {styles.calendarContainer}>
+        <Calendar
+          year= {2024}
+          month='March'
+        />
+      </View>
+      <View style = {styles.calendarContainer}>
+        <Calendar
+          year= {2024}
+          month='April'
+        />
+      </View>
+      <View style = {styles.calendarContainer}>
+        <Calendar
+          year= {2024}
+          month='May'
+        />
+      </View>
+      <View style = {styles.calendarContainer}>
+        <Calendar
+          year= {2024}
+          month='June'
+        />
+      </View>
+      <View style = {styles.calendarContainer}>
+        <Calendar
+          year= {2024}
+          month='July'
+        />
+      </View>
+      <View style = {styles.calendarContainer}>
+        <Calendar
+          year= {2024}
+          month='August'
+        />
+      </View>
+      <View style = {styles.calendarContainer}>
+        <Calendar
+          year= {2024}
+          month='September'
+        />
+      </View>
+      <View style = {styles.calendarContainer}>
+        <Calendar
+          year= {2024}
+          month='October'
+        />
+      </View>
+      <View style = {styles.calendarContainer}>
+        <Calendar
+          year= {2024}
+          month='November'
+        />
+      </View>
+      <View style = {styles.calendarContainer}>
+        <Calendar
+          year= {2024}
+          month='December'
+        />
+      </View>
+      
+    </ScrollView >
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: '10%',
-    
-    height:'100%',
-    display:'flex',
-    backgroundColor: 'lightblue',
+    flexGrow: 0,
     alignItems:'center',
+    paddingTop:'10%',
+    paddingBottom:'30%',
+    
   },
   text: {
     
   },
   calendarContainer:{
-    width:'80%',
-    backgroundColor:'red'
+    width:'90%',
+    backgroundColor:'white',
+    height: 350,
+    marginBottom:0,
   }
 });
