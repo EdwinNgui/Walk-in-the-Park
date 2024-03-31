949b9d48945c00a377375a1220fbc7029d32e5c6
Edwin <edwin.nguim@gmail.com>
2024-03-30 21:50:38 -0400

    Animation Pin

--- proj/assets/Pages/Map.js
+++ proj/assets/Pages/Map.js
@@ -1,74 +1,85 @@
-import React from 'react';
-import MapView, {Marker} from 'react-native-maps';
-import { StyleSheet, View , Button, Text} from 'react-native';
+import React, { useState, useRef } from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, View, Button, Text } from 'react-native';
 import * as Location from 'expo-location';
-import { useState } from 'react';
-import axios from 'axios';
 
 export default function MapComponent() {
   const [latitude, setLatitude] = useState(0);
   const [longitude, setLongitude] = useState(0);
-  const [latlng, setLatlng] = useState({ latitude: 0 , longitude: 0});
-  const [locStatus, setLocStatus] = useState(false)
+  const [locStatus, setLocStatus] = useState(false);
+  const mapRef = useRef(null);
+
   const getLocation = async () => {
     let { status } = await Location.requestForegroundPermissionsAsync();
-    console.log(`Status: ${status}`);
     if (status !== 'granted') {
       console.log('Permission to access location was denied');
-    }
-    else{  
-      let location = await Location.getLastKnownPositionAsync ({});
+    } else {
+      let location = await Location.getLastKnownPositionAsync({});
       setLocStatus(true);
       setLatitude(location.coords.latitude);
       setLongitude(location.coords.longitude);
-      setLatlng({latitude:location.coords.latitude, longitude:location.coords.longitude});
-      console.log(`Location: ${location.coords.latitude} ${location.coords.longitude}`);
+      mapRef.current.animateToRegion(
+        {
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.0922, // adjust these values as needed for zoom level
+          longitudeDelta: 0.0421, // adjust these values as needed for zoom level
+        },
+        1500 // duration in milliseconds
+      );
     }
-  }
-  
+  };
+
   return (
     <View style={styles.container}>
-      <View style = {styles.mapContainer}>
-        <MapView style={styles.map}>
-          {locStatus ? <Marker coordinate={latlng} title="Marker Title" description="Marker Description" /> : <></>}
+      <View style={styles.mapContainer}>
+        <MapView style={styles.map} ref={mapRef}>
+          {locStatus ? (
+            <Marker
+              coordinate={{
+                latitude: latitude,
+                longitude: longitude,
+              }}
+              title="Marker Title"
+              description="Marker Description"
+            />
+          ) : null}
         </MapView>
       </View>
-      <View style = {styles.buttonContainer}>
-        <View style = {styles.getLocationContainer}>
-          <Text onPress={getLocation} style={styles.button}>Location</Text>
+      <View style={styles.buttonContainer}>
+        <View style={styles.getLocationContainer}>
+          <Text onPress={getLocation} style={styles.button}>
+            Location
+          </Text>
         </View>
       </View>
-    
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    height: '100%'
+  container: {
+    height: '100%',
   },
-  mapContainer:{
-    height:'80%',
+  mapContainer: {
+    height: '80%',
   },
-  map:{
-    height:'100%'
+  map: {
+    height: '100%',
   },
-  buttonContainer:{
-    height:'20%',
-    display:'flex',
+  buttonContainer: {
+    height: '20%',
+    display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    
   },
-  button:{
-    
+  getLocationContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    height: '100%',
+    backgroundColor: 'blue',
   },
-  getLocationContainer : {
-    display:'flex',
-    justifyContent:'center',
-    alignItems:'center',
-    textAlign:'center',
-    height:'100%',
-    backgroundColor:'blue'
-  },
+  button: {},
 });
